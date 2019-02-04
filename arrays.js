cda3f2009fa64e027598deacbf54b25a8a025c71
Fer Hernandez <fh.1111@gmail.com>
2019-02-04 02:59:01 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -28,6 +28,6 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
   return array
 }
 
-function destructivelyRemoveElementFromBeginningOfArray(array){
-  array.slice()
+function removeElementFromBeginningOfArray(array){
+  array.slice(1)
 }
