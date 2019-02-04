d4da93ebb666d21c90766b8014a943421eec17c5
Fer Hernandez <fh.1111@gmail.com>
2019-02-04 03:02:16 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -37,5 +37,5 @@ function destructivelyRemoveElementFromEndOfArray(array){
 }
 
 function removeElementFromEndOfArray(array){
-  array.slice(0, array.lenght -1)
+  return array.slice(0, array.lenght -1)
 }
